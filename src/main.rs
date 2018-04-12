e1624145f774f2abc76d8a85536f9d7164101983
ZapAnton <wc3ft2@gmail.com>
2018-04-12 18:07:50 +0300

    Getting user input for main menu

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-fn show_welcome_message() -> Result<i32, std::io::Error> {
+use std::{io, io::Write};
+
+fn show_welcome_message() -> Result<u32, std::io::Error> {
     println!("Welcome to Intencja Game!");
 
     print!(
         "Press 1 to start a new game.\n\
-        Press 2 to enter game settings.\n\
-        Press 3 to read game tutorial\n\
-        Press 4 to exit game.\n>"
+         Press 2 to enter game settings.\n\
+         Press 3 to read game tutorial\n\
+         Press 4 to exit game.\n>"
     );
 
-    //TODO Get user input
-    
-    Ok(1)
+    io::stdout().flush();
+
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read user input!");
+
+    let user_input: u32 = user_input
+        .trim()
+        .parse()
+        .expect("Failed to parse input. You must enter a number!");
+
+    Ok(user_input)
 }
 
 fn start_new_game() {
@@ -29,7 +42,7 @@ fn exit_game() {
     println!("Exiting game!");
 }
 
-fn process_option(chosen_option: i32) {
+fn process_option(chosen_option: u32) {
     match chosen_option {
         1 => start_new_game(),
         2 => show_settings(),
