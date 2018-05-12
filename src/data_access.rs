8f09cbb0b7666d5eeba24c68ea2808817b96a845
ZapAnton <wc3ft2@gmail.com>
2018-05-12 13:28:06 +0300

    Replaced io::Read::read_to_string with fs::read_to_string as of Rust 1.26

--- src/data_access.rs
+++ src/data_access.rs
@@ -1,15 +1,7 @@
-use std::{fs::File, io::{Error, Read}, path::Path};
+use std::{fs, io::Error};
 
 pub fn get_text_data(dir: &str, filename: &str) -> Result<String, Error> {
-    let mut file_content = String::new();
+    let data_path = format!("data/text/{}/{}.txt", dir, filename);
 
-    let data_path_str = format!("data/text/{}/{}.txt", dir, filename);
-
-    let data_path = Path::new(&data_path_str);
-
-    let mut data_file = File::open(&data_path)?;
-
-    data_file.read_to_string(&mut file_content)?;
-
-    Ok(file_content)
+    Ok(fs::read_to_string(&data_path)?)
 }
