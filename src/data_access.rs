2becdd21c807568496e22292cba682d43187cfc6
ZapAnton <wc3ft2@gmail.com>
2019-01-29 17:21:09 +0300

    Begin again

--- src/data_access.rs
+++
@@ -1,7 +0,0 @@
-use std::{fs, io::Error};
-
-pub fn get_text_data(dir: &str, filename: &str) -> Result<String, Error> {
-    let data_path = format!("data/text/{}/{}.txt", dir, filename);
-
-    Ok(fs::read_to_string(&data_path)?)
-}
